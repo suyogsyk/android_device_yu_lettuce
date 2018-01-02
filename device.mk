5099e573f44794948f2ea0fae797904e91bfa653
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-02 16:05:51 -0500

    lettuce: Set IMS to use CodeAurora extension
    Change-Id: I64cba319986748e4dad206ca664ccda7eefb348a

--- device.mk
+++ device.mk
@@ -124,6 +124,14 @@ PRODUCT_COPY_FILES += \
 # System Properties
 $(call inherit-product, device/yu/lettuce/system_prop.mk)
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
