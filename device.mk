5408e0bb6362ee4548f291baaa35804aa0c22546
Mangomanid <skgaming7869@gmail.com>
2020-01-03 12:53:15 +0530

    Revert "lettuce: Enable FEATURE_TELEPHONY_IMS for these devices"
    This reverts commit 0f55bcf83f093798e8c11a2246fd44871df1d1d5.

--- device.mk
+++ device.mk
@@ -108,10 +108,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/qmi_config.xml \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/dsi_config.xml
 
-# Recorder
-PRODUCT_PACKAGES += \
-    Recorder
-
 # Sensors
 PRODUCT_PACKAGES += \
     calmodule.cfg \
