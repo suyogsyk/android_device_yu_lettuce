f2efcfbf407c1aa65bb4079cc2be6fa24919585d
Mangomanid <skgaming7869@gmail.com>
2020-03-23 01:00:44 +0530

    lettuce: Build Permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,6 +53,7 @@ BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
 LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_CONFIG := lineageos_lettuce_defconfig
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-0040/double_tap_enable"
